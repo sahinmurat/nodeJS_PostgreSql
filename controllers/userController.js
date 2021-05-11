1369714a1b762045ca4c30f228b26720f102f538
Murat Sahin <yazilimogren1111@gmail.com>
2021-05-11 23:28:50 +0200

    update function is creared

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,3 +38,33 @@ try {
       console.log(error)
     }
   }
+
+  exports.edit_user =  async (req, res) =>{
+    const userList =  await UserModel.findAll();
+   res.render('changeUser',{userList,id:req.params.id})
+  }
+  
+  exports.edit_2_user = async (req, res) =>{
+    try {
+      await UserModel.destroy({
+        where:{
+          id : req.params.id
+        }
+      })
+      // res.send('/users')
+    } catch (error) {
+      res.send('ilk adim hatasi')
+    }
+  
+    try {
+      const newUser = await UserModel.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      })
+      const userList =  await UserModel.findAll();
+      // res.render('index');
+      res.redirect('/users')
+       } catch (error) {
+      res.send('ikinci adim hatasi')
+      }
+  }
--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,7 @@ router.get('/', userController.get_user);
 router.get('/add', userController.add_user_form);
 router.post('/add', userController.add_user);
 router.get('/:id/delete', userController.delete_user)
+router.get('/:id/edit', usersController.edit_user);
+router.post('/:id/change', usersController.edit_2_user);
 
 module.exports = router;
